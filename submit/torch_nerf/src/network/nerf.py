727767a5bba9664728a5916cd1998d06031e4297
sjwoo0612 <sjwoo0612@kaist.ac.kr>
2025-04-25 14:57:29 +0000

    submit

---
+++ submit/torch_nerf/src/network/nerf.py
@@ -0,0 +1,100 @@
+"""
+Pytorch implementation of MLP used in NeRF (ECCV 2020).
+"""
+
+from typing import Tuple
+from typeguard import typechecked
+
+from jaxtyping import Float, jaxtyped
+import torch
+import torch.nn as nn
+
+
+class NeRF(nn.Module):
+    """
+    A multi-layer perceptron (MLP) used for learning neural radiance fields.
+
+    For architecture details, please refer to 'NeRF: Representing Scenes as
+    Neural Radiance Fields for View Synthesis (ECCV 2020, Best paper honorable mention)'.
+
+    Attributes:
+        pos_dim (int): Dimensionality of coordinate vectors of sample points.
+        view_dir_dim (int): Dimensionality of view direction vectors.
+        feat_dim (int): Dimensionality of feature vector within forward propagation.
+    """
+
+    def __init__(
+        self,
+        pos_dim: int,
+        view_dir_dim: int,
+        feat_dim: int = 256,
+    ) -> None:
+        """
+        Constructor of class 'NeRF'.
+        """
+        super().__init__()
+
+        # TODO
+        self.fc = nn.Sequential(
+            nn.Linear(pos_dim, feat_dim),
+            nn.ReLU(),
+            nn.Linear(feat_dim, feat_dim),
+            nn.ReLU(),
+            nn.Linear(feat_dim, feat_dim),
+            nn.ReLU(),
+            nn.Linear(feat_dim, feat_dim),
+            nn.ReLU(),
+            nn.Linear(feat_dim, feat_dim),
+        )
+
+        self.fc_skip1 = nn.Sequential(
+            nn.Linear(feat_dim + pos_dim, feat_dim),
+            nn.ReLU(),
+            nn.Linear(feat_dim, feat_dim),
+            nn.ReLU(),
+            nn.Linear(feat_dim, feat_dim),
+            nn.ReLU(),
+            nn.Linear(feat_dim, feat_dim),
+        )
+        self.sigma = nn.Sequential(nn.Linear(feat_dim, 1), nn.Softplus())
+        self.color = nn.Sequential(
+            nn.Linear(feat_dim + view_dir_dim, feat_dim // 2),
+            nn.ReLU(),
+            nn.Linear(feat_dim // 2, 3),
+            nn.Sigmoid(),
+        )
+
+    @jaxtyped
+    @typechecked
+    def forward(
+        self,
+        pos: Float[torch.Tensor, "num_sample pos_dim"],
+        view_dir: Float[torch.Tensor, "num_sample view_dir_dim"],
+    ) -> Tuple[
+        Float[torch.Tensor, "num_sample 1"], Float[torch.Tensor, "num_sample 3"]
+    ]:
+        """
+        Predicts color and density.
+
+        Given sample point coordinates and view directions,
+        predict the corresponding radiance (RGB) and density (sigma).
+
+        Args:
+            pos: The positional encodings of sample points coordinates on rays.
+            view_dir: The positional encodings of ray directions.
+
+        Returns:
+            sigma: The density predictions evaluated at the given sample points.
+            radiance: The radiance predictions evaluated at the given sample points.
+        """
+
+        # TODO
+        # pos = pos.to("cuda:0")
+        x = self.fc(pos)
+        x = torch.cat([pos, x], dim=1)
+        x = self.fc_skip1(x)
+        sigma = self.sigma(x)
+        x = torch.cat([x, view_dir], dim=1)
+        radiance = self.color(x)
+
+        return sigma, radiance
